10bf7d7f421e9dee88e4ee1706da1c5ffa69ec7c
hao <hao@hao-2.local>
2022-10-22 17:09:35 +0700

    finished 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,23 @@
 /* eslint-disable */
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import {areTheseObjectsEqual} from './tools'
 import Filter from "./components/Filter"
+import axios from "axios"
 import AddNew from "./components/AddNew"
 import Persons from "./components/Persons"
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [personsCopy, setPersonsCopy] = useState(persons)
+  const [persons, setPersons] = useState([])
+  const [personsCopy, setPersonsCopy] = useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:3001/persons').then((res)=>{
+      setPersons(persons.concat(res.data))
+      setPersonsCopy(persons.concat(res.data))
+    })
+  },[])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+  
   const compare = (arr,obj)=>{
     let bool = false
     for (const item of arr) {
